4f8707da909b099862e4e5f38e0c21805dcad508
nvsriram <nuranisri01@gmail.com>
2024-06-06 02:47:16 -0400

    Use component in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,6 @@
 
 import { UserProfileIcon } from "@dynamic-labs/iconic";
 import {
-  CopyIcon,
   Wallet,
   useDynamicContext,
   useIsLoggedIn,
@@ -10,10 +9,9 @@ import {
 } from "@dynamic-labs/sdk-react-core";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { BalanceSummary } from "../components/BalanceSummary";
 import { LogoutButton } from "../components/LogoutButton";
-import { NativeIcon } from "../components/NativeIcon";
-import { TransferButton } from "../components/TransferButton";
-import { displayAddress } from "../utils";
+import { BaseNFTGrid } from "../components/NFTGrid";
 
 const Dashboard = () => {
   const router = useRouter();
@@ -21,16 +19,20 @@ const Dashboard = () => {
   const userWallets = useUserWallets();
 
   const { user } = useDynamicContext();
-  const [wallets, setWallets] = useState<[Wallet, string | undefined][]>([]);
+  const [solanaWallet, setSolanaWallet] =
+    useState<[Wallet, string | undefined]>();
+  const [baseWallet, setBaseWallet] = useState<[Wallet, string | undefined]>();
 
   useEffect(() => {
     const handleWallets = async () => {
-      const wallets: [Wallet, string | undefined][] = [];
       for (const userWallet of userWallets) {
         const balance = await userWallet.connector.getBalance();
-        wallets.push([userWallet, balance]);
+        if (userWallet.chain === "eip155") {
+          setBaseWallet([userWallet, balance]);
+        } else {
+          setSolanaWallet([userWallet, balance]);
+        }
       }
-      setWallets(wallets);
     };
     handleWallets();
   }, [userWallets]);
@@ -41,43 +43,38 @@ const Dashboard = () => {
   }
 
   return (
-    <div className="flex min-h-screen flex-row items-center justify-center p-24">
-      <div className="flex w-full flex-col gap-3 items-center justify-start">
-        <div className="flex flex-col gap-3 w-full rounded-lg bg-dynamic-base-1 border border-dynamic-base-4 p-5">
-          <div className="flex flex-row w-full gap-3 p-2 rounded-lg text-base bg-dynamic-button-primary-background">
-            <UserProfileIcon height={24} width={24} />
-            {user.email}
+    <div className="flex min-h-screen flex-row justify-center">
+      <div className="flex h-full w-full flex-col items-center justify-start p-5">
+        <div className="flex flex-col w-full py-5">
+          <div className="flex flex-row w-full justify-between bg-gradient-to-t from-dynamic-button-primary-background rounded-t-lg p-3 pb-6 border-b border-b-dynamic-base-3">
+            <div className="flex flex-col gap-3 text-2xl font-semibold">
+              <UserProfileIcon
+                height={128}
+                width={128}
+                className="border-4 border-black rounded-full"
+              />
+              {user.email}
+            </div>
+            <LogoutButton />
           </div>
-          <div className="flex flex-col w-full rounded-lg bg-dynamic-button-primary-background">
-            {wallets.map(([wallet, balance], idx) => {
-              return (
-                <div
-                  key={idx}
-                  className="flex flex-row gap-1 p-3 border-b border-b-dynamic-base-3"
-                >
-                  <div className="flex w-full gap-2 text-base flex-row items-center justify-between">
-                    <button
-                      className="flex text-dynamic-text-secondary gap-2 hover:text-white focus:text-white active:text-white h-full w-full text-sm"
-                      onClick={() =>
-                        navigator.clipboard.writeText(wallet.address)
-                      }
-                    >
-                      {displayAddress(wallet.address, 7)}
-                      <CopyIcon />
-                    </button>
-                    <NativeIcon
-                      chain={wallet.chain}
-                      balance={balance}
-                      connectedChain={wallet.connector.connectedChain}
-                    />
-                  </div>
-                </div>
-              );
-            })}
+          <div className="flex flex-col w-full rounded-b-lg bg-dynamic-button-primary-background">
+            {solanaWallet && (
+              <BalanceSummary
+                isFirst
+                wallet={solanaWallet[0]}
+                balance={solanaWallet[1]}
+              />
+            )}
+            {baseWallet && (
+              <BalanceSummary wallet={baseWallet[0]} balance={baseWallet[1]} />
+            )}
           </div>
-          <TransferButton />
         </div>
-        <LogoutButton />
+        <hr className="w-full border border-dynamic-base-4" />
+        <div className="flex flex-col gap-5 w-full py-5">
+          <h1 className="text-xl">Owned NFTs</h1>
+          <BaseNFTGrid wallet={baseWallet ? baseWallet[0] : undefined} />
+        </div>
       </div>
     </div>
   );
